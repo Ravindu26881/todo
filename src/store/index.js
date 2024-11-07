263edf4c5067f61224c071bd0d67f58c7aaa6337
Ravindu <85988392+Ravindu26881@users.noreply.github.com>
2024-11-07 06:19:21 +0530

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -16,7 +16,13 @@ export default createStore({
         id: Math.floor(10000 + Math.random() * 90000),
         title: "Mark completed tasks by clicking the ✓ icon",
         completed: true
-      }]
+      },
+      {
+        id: Math.floor(10000 + Math.random() * 90000),
+        title: "Move up high priority tasks by dragging the task to the top of the list.",
+        completed: false
+      }
+    ]
   },
   getters: {
     getTaskByTitle: (state) => (searchString) => {
